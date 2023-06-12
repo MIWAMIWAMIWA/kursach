6b4f9cbab459f7ffa099e2098e9bcdbd4f4736e5
MIWAMIWAMIWA <107461633+MIWAMIWAMIWA@users.noreply.github.com>
2023-06-13 00:23:48 +0300

    no tests for now

---
+++ src/test/java/ua/lviv/iot/algo/part1/fileManager/AutomobileWriterTest.java
@@ -0,0 +1,24 @@
+package ua.lviv.iot.algo.part1.fileManager;
+
+import org.junit.jupiter.api.Test;
+import ua.lviv.iot.algo.part1.model.Automobile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AutomobileWriterTest {
+
+
+    @Test
+    void writeAutomobile() {
+    }
+
+    @Test
+    void savingLastId() {
+
+    }
+
+    @Test
+    void getFilesFromDirectory() {
+    }
+}
